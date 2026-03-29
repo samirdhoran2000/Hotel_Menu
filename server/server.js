448dfb742237db209eb1098b4790797081d74588
SAMIR DHORAN <samirdhoran2000@gmail.com>
2026-03-29 21:29:58 +0530

    update

--- server/server.js
+++ server/server.js
@@ -1,18 +1,14 @@
 // src/index.js
-import fs from "node:fs";
-import https from "https";
+import http from "http";
 
 import app from "./app.js";
 import { config } from "./config/config.js";
 
 // HTTPS options
-const options = {
-  key: fs.readFileSync(config.sslKeyPath),
-  cert: fs.readFileSync(config.sslCertPath),
-};
+
 
 // Create HTTPS server
-const server = https.createServer(options, app);
+const server = http.createServer(app);
 
 // Start server
 // Import sequelize
@@ -22,7 +18,7 @@ import sequelize from "./models/index.js";
 sequelize.sync({ alter: true }).then(() => {
   console.log("Database synchronized");
   server.listen(config.port, config.host, () => {
-    console.log(`Server running on https://${config.host}:${config.port}`);
+    console.log(`Server running on http://${config.host}:${config.port}`);
   });
 }).catch((err) => {
   console.error("Failed to sync database:", err);
